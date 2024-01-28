e7bc42b1b5908870b3e35fe97d656ca88815508c
DinhQuangKhanh <khanhdqde170745@fpt.edu.vn>
2024-01-28 11:28:42 +0700

    J1.S.P0025

---
+++ J1.S.P0025/src/view/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author ADMIN
+ */
+import Controller.Nomalize;
+public class Main {
+
+    public static void main(String[] args) {
+        Nomalize.readAndWrite();
+    }
+    
+}
